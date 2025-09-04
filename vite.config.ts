91ce76bbad390e51cf4155b5cf50f3f20137d944
Jaskaran <thecodersingh@gmail.com>
2025-09-04 01:45:22 -0700

    Fix module error

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
-import eslint from 'vite-plugin-eslint';
+import eslintPlugin from '@nabla/vite-plugin-eslint';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    eslint({
-      emitWarning: true, // emits to the console if there is a warning
-      emitError: true, // emits to the console if there is an error
-      failOnError: false, // hides the stack trace
-    }),
-  ],
+  plugins: [react(), tailwindcss(), eslintPlugin({})],
 });
