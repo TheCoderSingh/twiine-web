d3ad8c9ece955bd9225e9d4ad7638f116afad00e
Jaskaran <thecodersingh@gmail.com>
2025-09-06 17:32:45 -0700

    Build How it Works section, Fix title styling, Create StepCard component

--- src/components/Title/index.tsx
+++ src/components/Title/index.tsx
@@ -9,7 +9,7 @@ const Title = ({ icon, text }: TitleProps) => {
   const Icon = Icons[icon] as React.ElementType;
 
   return (
-    <div className="flex justify-center items-center gap-2 bg-light w-[120px] rounded-[100px] px-16 py-2 mx-auto">
+    <div className="inline-flex justify-center items-center gap-2 bg-light rounded-[100px] px-4 py-2 mx-auto">
       <div>{<Icon fontSize={12} />}</div>
       <div>
         <h2 className="text-heading text-lg font-[500]">{text}</h2>
